6aea4c9908c7212bfacd141752c95fbc177e49fa
“djacquel” <david.jacquel@gmail.com>
2023-07-24 15:07:45 +0200

    tina cms files

---
+++ tina/config.ts
@@ -0,0 +1,65 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+    basePath:     ".",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "_posts",
+        ui: {
+          filename: {
+            readonly: false,
+            slugify: values => {
+              const date = new Date();
+              const day = date.getDate().toString().padStart(2, '0');
+              let month = date.getMonth() + 1;
+              month = month .toString() .padStart(2, '0');
+              const year = date.getFullYear();
+              let currentDate = `${year}-${month}-${day}`;
+              return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
+            }
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+          },
+        ],
+      },
+    ],
+  },
+});
